5e50b98bcef0c1b1871b2d5ce6470d3fa03696fa
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2021-04-26 21:13:55 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,11 +12,17 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1.1",
-	name: "Literally nothing, kinda",
+	num: "0.2",
+	name: "DOUBLE (almost) EVERYTHING!",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
+	<h3>v0.2</h3><br>
+        - Radiowave Layer is finished.<br>
+		- Three challenges were added in.<br>
+		- Three more functional version milestones were added in.<br>
+		- Two more upgrades were added in.<br>
+		- Changed some titles and descriptions (again).<br>
 	<h4>v0.1.1</h3>
 		- Changed some titles and descriptions to make more sense.<br>
 		- Made it so 6th milestone wouldn't appear after achieving 4th.<br>
@@ -24,7 +30,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Mod was created on April 10th with two layers.<br>
 		- Garbage mechanic is introduced to make this thing possible.`
 
-let winText = `Congrats for beating TET v0.1... Yep, that's all there is for now.`
+let winText = `Congrats for beating TET v0.2... Yep, that's all there is for now. Don't think you can farm versions now jajajajajajajajaja-`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
@@ -44,23 +50,35 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-
+	
+	let baseSoftcapStrength = new Decimal(1)
+	if(inChallenge("rw", 12) || inChallenge("rw", 22)) baseSoftcapStrength = new Decimal(9)
     let base = new Decimal(100).div(player.ou.garbageLevel.mul(10))
 	if(hasMilestone("ou", 3)) base = new Decimal(100).div(player.ou.garbageLevel.mul(12.5))
 	if(inChallenge("rw", 11)) base = base.div(player.ou.garbageLevel.mul(12.5))
 	let gain = new Decimal(player.points.div(base))
+	if(hasUpgrade("ou", 14)) gain = gain.mul(2)
+	if(hasUpgrade("ou", 15)) gain = gain.mul(player.ou.garbagePoints.add(20).log(20))
 	if(hasMilestone("ou", 4)) gain = gain.mul(new Decimal(player.ou.milestones.length).root(3))
-	if(player.points.gte(2.99792458)) gain = gain.div(player.points.root(21))
-	if(player.points.gte(29.9792458)) gain = gain.div(player.points.root(20))
-	if(player.points.gte(299.792458)) gain = gain.div(player.points.root(19))
-	if(player.points.gte(2997.92458)) gain = gain.div(player.points.root(18))
-	if(player.points.gte(29979.2458)) gain = gain.div(player.points.root(17))
-	if(player.points.gte(299792.458)) gain = gain.div(player.points.root(16))
-	if(player.points.gte(2997924.58)) gain = gain.div(player.points.root(15))
-	if(player.points.gte(29979245.8)) gain = gain.div(player.points.root(14))
-	if(player.points.gte(299792458)) gain = gain.div(player.points.root(13))
-	if(inChallenge("rw", 11) && player.points.gte(1)) gain = gain.div(player.points.root(2))
+	if(hasMilestone("ou", 5)) gain = gain.mul(new Decimal(player.ou.upgrades.length).root(2))
+	if(hasMilestone("ou", 6)) gain = gain.mul(new Decimal(player.ou.garbagePoints).root(10))
+	if(hasChallenge("rw", 22)) gain = gain.mul(9001)
+	if(player.points.gte(2.99792458)) gain = gain.div(player.points.root(new Decimal(21).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(29.9792458)) gain = gain.div(player.points.root(new Decimal(20).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(299.792458)) gain = gain.div(player.points.root(new Decimal(19).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(2997.92458)) gain = gain.div(player.points.root(new Decimal(18).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(29979.2458)) gain = gain.div(player.points.root(new Decimal(17).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(299792.458)) gain = gain.div(player.points.root(new Decimal(16).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(2997924.58)) gain = gain.div(player.points.root(new Decimal(15).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(29979245.8)) gain = gain.div(player.points.root(new Decimal(14).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(299792458)) gain = gain.div(player.points.root(new Decimal(13).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(2997924580)) gain = gain.div(player.points.root(new Decimal(12).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(29979245800)) gain = gain.div(player.points.root(new Decimal(11).sub(1).div(baseSoftcapStrength).add(1)))
+	if(player.points.gte(299792458000)) gain = gain.div(player.points.root(new Decimal(10).sub(1).div(baseSoftcapStrength).add(1)))
+	if((inChallenge("rw", 11) || inChallenge("rw", 22)) && player.points.gte(1)) gain = gain.div(player.points.root(new Decimal(2).sub(1).div(baseSoftcapStrength).add(1)))
+	if(inChallenge("rw", 12)) gain = gain.mul(3)
 	if(player.points.gte(2.99792458) && (!hasChallenge("rw", 11) && !inChallenge("rw", 11))) gain = new Decimal(0)
+	if(player.points.gte(2997924580)) gain = new Decimal(0)
 	return gain
 }
 
@@ -75,7 +93,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return hasMilestone("ou", 5)
+	return hasMilestone("mw", 0)
 }
 
 
